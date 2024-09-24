7ed51e39be36764f981fa534778e2eb8290985f5
correctforward818 <stayforward0513@gmail.com>
2024-09-24 00:49:38 -0700

    firstcomponent

--- src/index.js
+++ src/index.js
@@ -153,35 +153,35 @@ const tryConverter = (temperature, convert) => {
     const rounded = Math.round(output * 1000) / 1000;
     return rounded.toString();
 }
-// const Blog = (props) => {
-//     const sideBar = (
-//         <ul>
-//             {props.posts.map((post) => 
-//                 <li key={post.id}>
-//                     {post.title}
-//                 </li>
-//             )}
-//         </ul>
-//     );
+const Blog = (props) => {
+    const sideBar = (
+        <ul>
+            {props.posts.map((post) => 
+                <li key={post.id}>
+                    {post.title}
+                </li>
+            )}
+        </ul>
+    );
 
-//     const content = props.posts.map((post) => 
-//         <div key={post.id}>
-//             <h3>{post.title}</h3>
-//             <p>{post.content}</p>
-//         </div>
-//     );
-//     return(
-//         <div>
-//             {sideBar}
-//             <br />
-//             {content}
-//         </div>
-//     );
-// }
+    const content = props.posts.map((post) => 
+        <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+        </div>
+    );
+    return(
+        <div>
+            {sideBar}
+            <br />
+            {content}
+        </div>
+    );
+}
 
-// const posts = [
-//     {id: 1, title: "Hello world!", content: "Welcome to learning React!" },
-//     {id: 2, title: "Installation", content: "You can install React from npm"}
-// ]
+const posts = [
+    {id: 1, title: "Hello world!", content: "Welcome to learning React!" },
+    {id: 2, title: "Installation", content: "You can install React from npm"}
+]
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Calculator />);
